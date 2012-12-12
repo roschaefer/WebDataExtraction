ad10f4294f4fa2b69370ea881908c84f8db13ac3
Robert Schaefer <robert.schaefer@student.hpi.uni-potsdam.de>
2012-12-12 01:20:04 +0100

    compare.rb has now     - reasonable command line parsing     - input/output file parameters still missing: outputting result to stdout if outfile is not given

--- Exercise5/compare.rb
+++ Exercise5/compare.rb
@@ -1,18 +1,39 @@
 require 'csv'
 require 'levenshtein'
+require 'trollop'
 
 
+opts = Trollop::options do
+    version "Compare 1.0 (c) 2012 Robert Schaefer"
+    banner <<-EOS
+This program evaluates two string comparison algorithms:
+    Levenshtein Distance
+    Levenshtein Distance on Soundex
+Usage:
+    compare.rb [options] 
+where [options] are:
+EOS
+    opt :infile, "Input csv file", :type => String
+    opt :outfile, "Output csv file", :type => String
+end
+
+Trollop::die :infile, "must be given" unless opts[:infile] and File.exist?(opts[:infile]) 
 
 def soundexComparison(string1, string2)
     soundex1 = %x"./soundex #{string1}".split[1]
     soundex2 = %x"./soundex #{string2}".split[1]
-    return Levenshtein.distance(soundex1, soundex2)
+    return Levenshtein.normalized_distance(soundex1, soundex2)
 end
 
-CSV.open("./output.csv", "wb") do |csv|
-    CSV.foreach("./input.csv") do |row| 
-        lvst = Levenshtein.distance(row[0], row[1])
+def output(row)
+        lvst = Levenshtein.normalized_distance(row[0], row[1])
         soundex = soundexComparison(row[0], row[1])
-        csv << [row[0], row[1], lvst, soundex]
+        return [row[0], row[1], lvst, soundex]
+end
+
+
+CSV.open(opts[:outfile], "wb") do |csv|
+    CSV.foreach(opts[:infile]) do |row| 
+        csv << output(row)
     end
 end
